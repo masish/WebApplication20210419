ee93648c4b4b27510e5220281404d4e3aaea249f
maishii <haru_5175@yahoo.co.jp>
2021-05-10 12:56:13 +0900

    Test

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication20210419.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
